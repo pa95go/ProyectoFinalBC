571be8209978b781fdfd9c82ab081996aedaf11d
Pablo Gomez <pa95go@gmail.com>
2021-02-11 14:14:46 +0100

    Menu responsive Player

--- AplicacionFinal/sponsor360/src/component/player/menu.js
+++ AplicacionFinal/sponsor360/src/component/player/menu.js
@@ -8,20 +8,20 @@ function Menu (){
     return(
     
     <div className ="menu-content"  >
-        <span className ='logo'>sponsor360º</span>
+        <span className ='logo'></span>
+        
         
         <div className="panel">
-            <div className="photo">
-                <div className="image"></div>
-            </div>
+            <div className="image"></div>
+       
             <h3 className = 'name'>NOMBRE APELLIDO </h3>
             <h5 className='deporte'>DEPORTE</h5>
         <div className="sections">
             <ul>
-                <li><button className="btn-menu">Inicio</button> </li>
-                <li><button className="btn-menu">Perfil</button></li>
-                <li><button className="btn-menu">Soportes</button></li>
-                <li><button className="btn-menu">Marcas</button></li>
+                <li><button className="btn-menu"><span>&nbsp;</span><i class="icon ion-md-home"></i><span > Inicio</span> </button> </li>
+                <li><button className="btn-menu"><span>&nbsp;</span><i class="icon ion-md-person"></i><span> Perfil</span></button></li>
+                <li><button className="btn-menu"><span>&nbsp;</span><i class="icon ion-md-cube"></i> <span> Soportes</span></button></li>
+                <li><button className="btn-menu"><span>&nbsp;</span><i class="icon ion-md-star"></i><span> Marcas</span></button></li>
             </ul>
         </div>
         </div>
