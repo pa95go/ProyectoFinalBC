4080729e7899f6a0e45d7e6ff850e356a7ef702a
Pablo Gomez <pa95go@gmail.com>
2021-02-17 11:10:55 +0100

    State button Register

--- AplicacionFinal/sponsor360/src/component/start/component-register/register-player.js
+++ AplicacionFinal/sponsor360/src/component/start/component-register/register-player.js
@@ -17,14 +17,17 @@ function RegisterPlayer (){
            <input type="text" name="" id="" placeholder='Email' className='reg-text-input reg-style-blue'/>
            <input type="password" name="" id="" placeholder='Contraseña' className='reg-text-input reg-style-blue'/>
            <input type="text" name="" id="" placeholder='Deporte' className='reg-text-input reg-style-blue '/>
+           <div className='width-full-component'>
+
            <input type="date" name="" id="" placeholder='Fecha de Nacimiento' className ='reg-style-date' />
            <select name="transporte"className="reg-select-sex" >
-                <option selected disabled>Sexo</option>
+                <option selected disabled>Sexo</option> {/* poner fondo oscuro y en fecha tb */}
                 <option value="Hombre">Hombre</option>
                 <option value="Mujer">Mujer</option>
                 <option value="Otro">Otro</option>
 
             </select>
+           </div>
             <input type="submit" value='Registrar Deportista' className='reg-btn-blue'/>
 
 
--- AplicacionFinal/sponsor360/src/component/start/register.js
+++ AplicacionFinal/sponsor360/src/component/start/register.js
@@ -9,10 +9,8 @@ function Register (){
 
      const [register, setRegister] = useState(<RegisterPlayer/>); 
     
-    /* function sayHello(){
-        register = <RegisterPlayer/>
-      }
- */
+   
+  
     return(
     
     <div className ="register-content"  >
@@ -22,9 +20,10 @@ function Register (){
 
            <dir className=' width-full-r'>
 
-            <button className='btn-blue ' /* onClick={sayHello()} */><i class="icon ion-md-tennisball"></i> Deportista</button>
-             <button className='btn-red '><i class="icon ion-md-planet"></i> Marca</button>
+            <button className='btn-blue '   onClick={()=>setRegister(<RegisterPlayer/> )}  ><i class="icon ion-md-tennisball"></i> Deportista</button>
+             <button className='btn-red ' onClick={()=>setRegister(<RegisterBrand/> )}><i class="icon ion-md-planet"></i> Marca</button>
 
+           
            </dir>
            <div >
                 {register} 
