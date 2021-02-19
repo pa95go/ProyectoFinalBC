b989d0e37c6e4013697700e126bc0bfaa8fa5ff7
Pablo Gomez <pa95go@gmail.com>
2021-02-19 12:58:54 +0100

    Inicio Fron Card terminadas

--- AplicacionFinal/sponsor360/src/App.js
+++ AplicacionFinal/sponsor360/src/App.js
@@ -20,7 +20,7 @@ function App() {
      
      
        </div>
-    {/* <MenuBrand/>   */}
+      <MenuBrand/>   
     
     </div>
   );
--- AplicacionFinal/sponsor360/src/component/player/component-player/inicio-player.js
+++ AplicacionFinal/sponsor360/src/component/player/component-player/inicio-player.js
@@ -14,35 +14,66 @@ function InicioPlayer (){
             <button>but2</button>
             lo que quiera
         </div>
+
+
      <h1 className = 'title-c blue-textcolor-c'> <i class="icon ion-md-home"></i> INICIO</h1>
      <hr/>
-     <div className="profile-inicio-c ">
-         <h2 className='title2-c '>ELENA PEREZ DE AYALA MARTINEZ</h2>
-         <h4 className='subtitle-c'>DEPORTE</h4>
-         <p className='text-c'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis laborum non nihil, 
+   
+     <div className="card-c cblue-c cw1-c ">
+         <h1 >PABLO GÓMEZ OSUNA</h1>
+         <p className='ctcenter-c'>DEPORTE</p>
+         <hr />
+         <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis laborum non nihil, 
              labore accusamus assumenda quisquam quibusdam quos, vitae, 
              velit ad porro architecto aspernatur omnis ratione sunt esse! Perspiciatis, voluptas.</p>
-         
      </div>
-     <div className='box-inicio-c'>
-        <div className="support-inicio-c">
-            <h2 >SOPORTE 1</h2>
-            <h4 className='subtitle-c red-textcolor-c'>CAMISETA POR DELANTE</h4>
-        </div>
-        <div className="support-inicio-c">
-            <h2 >SOPORTE 1</h2>
-            <h4 className='subtitle-c red-textcolor-c'>CAMISETA POR DELANTE</h4>
-        </div>
-        <div className="support-inicio-c">
-            <h2 >SOPORTE 1</h2>
-            <h4 className='subtitle-c red-textcolor-c'>CAMISETA POR DELANTE</h4>
-        </div>
-         
-     </div>
+    
+    <div className="box-c">
 
-        <div className="brand-inicio-c">
-            y aqui las marcas
+        <div className="card-c cblack-c cw3-c ">
+            <h1 >SOPORTE 1</h1>
+            <p className='ctcenter-c ctbold-c'>CAMISETA DELANTERA LOGO </p>
         </div>
+        <div className="card-c cblack-c cw3-c ">
+            <h1 >SOPORTE 2</h1>
+            <p className='ctcenter-c ctbold-c'>CAMISETA DELANTERA LOGO </p>
+        </div>
+        <div className="card-c cblack-c cw3-c ">
+            <h1 >SOPORTE 3</h1>
+            <p className='ctcenter-c ctbold-c'>CAMISETA DELANTERA LOGO </p>
+        </div>
+    </div>
+   
+    <div className="box-c">
+
+        <div className="card-c cred-c cw3-c ">
+            <h1 >MARCA 1</h1>
+            <p className='ctcenter-c ctbold-c'>NOMBRE LOGO </p>
+            <hr/>
+            <p>IMAGEN</p>
+            <p>IMAGEN</p>
+            <p>IMAGEN</p>
+        </div>
+        <div className="card-c cred-c cw3-c ">
+            <h1 >MARCA 2</h1>
+            <p className='ctcenter-c ctbold-c'>NOMBRE LOGO </p>
+            <hr/>
+            <p>IMAGEN</p>
+            <p>IMAGEN</p>
+            <p>IMAGEN</p>
+        </div>
+        <div className="card-c cred-c cw3-c ">
+            <h1 >MARCA 3</h1>
+            <p className='ctcenter-c ctbold-c'>NOMBRE LOGO </p>
+            <hr/>
+            <p>IMAGEN</p>
+            <p>IMAGEN</p>
+            <p>IMAGEN</p>
+        </div>
+    </div>
+
+
+
 
     </div>
     );
