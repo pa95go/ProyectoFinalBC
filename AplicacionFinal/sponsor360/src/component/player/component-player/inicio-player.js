28c22e3831cffde5333fad0d809499c1f69e7234
Pablo Gomez <pa95go@gmail.com>
2021-03-10 12:47:17 +0100

    Fron Player Terminado

--- AplicacionFinal/sponsor360/src/component/player/component-player/inicio-player.js
+++ AplicacionFinal/sponsor360/src/component/player/component-player/inicio-player.js
@@ -49,19 +49,25 @@ function InicioPlayer (){
         <div className="card-c cblue-c cw2-c ">
             <h1 >@</h1> 
             
-            <div className="box-c text-vertical-center-c m0-c">
-           <h2 className=' text-vertical-center-c text-light '> <i class="icon ion-logo-twitter blue-textcolor-c"></i> @pablo  <p className=' '>350   </p></h2>
-           
+            <div className=" box-noresponsive-c text-vertical-center-c m0-c">
+            
+           <h2 className=' text-vertical-center-c text-light '> <i class="icon ion-logo-twitter text-twitter"></i>   </h2>
+           <p className='ctbold-c '>@pablo   </p>
+           <p>2.7  <i class="icon ion-md-stats blue-textcolor-c"></i>  </p>
+            </div>
+         <hr/>
+            <div className="box-noresponsive-c text-vertical-center-c m0-c">
+            <h2 className=' text-vertical-center-c text-light '> <i class="icon ion-logo-facebook text-facebook"></i>   </h2>
+           <p className='ctbold-c '>@pablo   </p>
+          
+           <p >1.5  <i class="icon ion-md-stats blue-textcolor-c"></i>  </p>
             </div>
             <hr/>
-            <div className="box-c text-vertical-center-c m0-c">
-           <h2 className=' text-vertical-center-c text-light'> <i class="icon ion-logo-instagram blue-textcolor-c"></i> @pablo</h2>
-            <p className=' '>350  </p>
-            </div>
-            <hr/>
-            <div className="box-c text-vertical-center-c m0-c">
-           <h2 className=' text-vertical-center-c text-light'> <i class="icon ion-logo-facebook blue-textcolor-c"></i> @pablo</h2>
-            <p className=' '>350  </p>
+            <div className="box-noresponsive-c text-vertical-center-c m0-c">
+            <h2 className=' text-vertical-center-c text-light '> <i class="icon ion-logo-instagram text-instagram"></i>   </h2>
+           <p className='ctbold-c '>@pablo   </p>
+          
+           <p >0.8  <i class="icon ion-md-stats blue-textcolor-c"></i> </p>
             </div>
             <hr/>
         </div>
@@ -72,22 +78,22 @@ function InicioPlayer (){
     <div className="box-c">
         <div className="card-c cblack-c cw2-c ">
             <h1 ><i class="icon ion-md-shirt"></i></h1>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>Camiseta delantera </p>
            <h2 className=' text-vertical-center-c'><i class="icon ion-md-eye blue-textcolor-c"></i></h2>
             </div>
             <hr/>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>Camiseta delantera </p>
            <h2 className=' text-vertical-center-c'><i class="icon ion-md-eye-off "></i></h2>
             </div>
             <hr/>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>Camiseta delantera </p>
            <h2 className=' text-vertical-center-c'><i class="icon ion-md-planet red-textcolor-c"></i></h2>
             </div>
             <hr/>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>Camiseta delantera  </p>
             <h2 className=' text-vertical-center-c'><i class="icon ion-md-eye-off"></i></h2>
             </div>
@@ -98,20 +104,20 @@ function InicioPlayer (){
         <div className="card-c cred-c cw2-c ">
             <h1 ><i class="icon ion-md-planet"></i></h1>
 
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>COCACOLA </p>
             <h2 className=' text-vertical-center-c'><img className='img-xs-profile-red-c text-vertical-center-c' src='https://i.etsystatic.com/12396650/r/il/b9d997/2150011366/il_570xN.2150011366_8em7.jpg ' /></h2>
             </div>
             <hr/>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>Disponible </p>
             <h2 className=' text-vertical-center-c'><img className='img-xs-profile-red-c text-vertical-center-c' src='https://i.etsystatic.com/12396650/r/il/b9d997/2150011366/il_570xN.2150011366_8em7.jpg ' /></h2>            </div>
             <hr/>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>REDBULL </p>
             <h2 className=' text-vertical-center-c'><img className='img-xs-profile-red-c text-vertical-center-c' src='https://i.etsystatic.com/12396650/r/il/b9d997/2150011366/il_570xN.2150011366_8em7.jpg ' /></h2>            </div>
             <hr/>
-            <div className="box-c text-vertical-center-c">
+            <div className="box-noresponsive-c text-vertical-center-c">
             <p className='ctbold-c '>Disponible </p>
             <h2 className=' text-vertical-center-c'><img className='img-xs-profile-red-c text-vertical-center-c' src='https://i.etsystatic.com/12396650/r/il/b9d997/2150011366/il_570xN.2150011366_8em7.jpg ' /></h2>
             </div>
--- AplicacionFinal/sponsor360/src/component/player/component-player/perfil-player.js
+++ AplicacionFinal/sponsor360/src/component/player/component-player/perfil-player.js
@@ -37,9 +37,9 @@ function PerfilPlayer (){
      <h1 className = 'title-c blue-textcolor-c'> <i class="icon ion-md-person"></i> PERFIL</h1>
      <hr/>
    
+        <form action=""> 
      <div className="card-c cblue-c cw1-c ">
          <div className="banner-blue-c"></div>
-        <form action=""> 
       <button type='submit' className='btn-blue-c d-rigth margin-edit-c' onClick={(e)=>handleEdit(e)}><i class={handleIcon(edit)}></i></button> 
         <label for="file-input"  >
         <img className='img-profile-blue-c' src="https://cdn2.vectorstock.com/i/thumb-large/63/66/profile-placeholder-default-avatar-vector-21666366.jpg" disabled={edit}  />
@@ -62,11 +62,65 @@ function PerfilPlayer (){
         
         <textarea name="Descripcion" className='text-s' id="" placeholder='Descripción' disabled={edit}  ></textarea>
 
-       
-        </form>
-        
      </div>
-    
+
+     <div className="box-c">
+
+     
+
+     <div className="card-c cblue-c  cw3-c "> 
+        <h1 className='bg-twitter' ><i class="icon ion-logo-twitter "></i> </h1>
+        <input type="text" className='text-vertical-center-c text-bold text-xl ctcenter-c ' name="" id="twitter-input" placeholder='Email' placeholder="email" value='@pablo' disabled={edit}  />
+        <div className="box-noresponsive-c m0-c">
+            <p className=' text-vertical-center-c text-s mt10-c'>Seguidores:</p>
+            <p className=' text-vertical-center-c mt10-c' >10.589  <i class="icon ion-md-people blue-textcolor-c"></i> </p>
+        </div>
+       
+        <div className="box-noresponsive-c m0-c">
+            <p className=' text-vertical-center-c text-s m0-c'>Engagement:</p>
+            <p className=' text-vertical-center-c m0-c' >0.8  <i class="icon ion-md-stats blue-textcolor-c "></i> </p>
+        </div>
+        <hr/>
+     
+     </div>
+
+
+        
+     <div className="card-c cblue-c cw3-c "> 
+        <h1 className='bg-facebook'><i class="icon ion-logo-facebook "></i> </h1>
+        <input type="text" className='text-vertical-center-c text-bold text-xl ctcenter-c ' name="" id="twitter-input" placeholder='Email' placeholder="email" value='@pablo' disabled={edit}  />
+        <div className="box-noresponsive-c m0-c">
+            <p className=' text-vertical-center-c text-s mt10-c'>Seguidores:</p>
+            <p className=' text-vertical-center-c mt10-c' >10.589  <i class="icon ion-md-people blue-textcolor-c"></i> </p>
+        </div>
+       
+        <div className="box-noresponsive-c m0-c">
+            <p className=' text-vertical-center-c text-s m0-c'>Engagement:</p>
+            <p className=' text-vertical-center-c m0-c' >0.8  <i class="icon ion-md-stats blue-textcolor-c "></i> </p>
+        </div>
+        <hr/>
+     
+     </div>
+
+
+     <div className="card-c cblue-c cw3-c "> 
+        <h1 className='bg-instagram'><i class="icon ion-logo-instagram "></i> </h1>
+        <input type="text" className='text-vertical-center-c text-bold text-xl ctcenter-c ' name="" id="twitter-input" placeholder='Email' placeholder="email" value='@pablo' disabled={edit}  />
+        <div className="box-noresponsive-c m0-c">
+            <p className=' text-vertical-center-c text-s mt10-c'>Seguidores:</p>
+            <p className=' text-vertical-center-c mt10-c' >10.589  <i class="icon ion-md-people blue-textcolor-c"></i> </p>
+        </div>
+       
+        <div className="box-noresponsive-c m0-c">
+            <p className=' text-vertical-center-c text-s m0-c'>Engagement:</p>
+            <p className=' text-vertical-center-c m0-c' >0.8  <i class="icon ion-md-stats blue-textcolor-c "></i> </p>
+        </div>
+        <hr/>
+     
+     </div>
+  
+        </div>
+        </form>
 
     </div>
     );
